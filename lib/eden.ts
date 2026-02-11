507c415b1f6699bee7fbeca3d02d14b62f887d7f
Micheal <khanzaidan786@gmail.com>
2026-02-11 16:52:45 +0530

    Update eden.ts

--- lib/eden.ts
+++ lib/eden.ts
@@ -2,4 +2,4 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 
-export const client = treaty<App>('http://localhost:3000') 
+export const client = treaty<App>('https://private-chat.michealkhan.com/') 
